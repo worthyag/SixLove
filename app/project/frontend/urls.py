03d6d4e09ed263c674933c1e0eab8f6866287792
Worthy <worthyintech@gmail.com>
2024-01-23 19:03:17 +0000

    Completed the profile template.

--- app/project/frontend/urls.py
+++ app/project/frontend/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.landing, name="landing"),
-    # path("login/", views.login, name="login"),
-    # path("signup/", views.signup, name="signup"),
+    path("login/", views.login, name="login"),
+    path("signup/", views.signup, name="signup"),
     path("tennis/", views.tennis, name="tennis"),
     path("feed/", views.feed, name="feed"),
     path("learn/", views.learn, name="learn"),
