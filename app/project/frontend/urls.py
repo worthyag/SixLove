e3275f6c78842b45091eb0f7eefec925e100a336
Worthy <worthyintech@gmail.com>
2024-01-23 18:42:25 +0000

    Added all the urls and views.

--- app/project/frontend/urls.py
+++ app/project/frontend/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns = [
     path("", views.landing, name="landing"),
+    # path("login/", views.login, name="login"),
+    # path("signup/", views.signup, name="signup"),
     path("tennis/", views.tennis, name="tennis"),
+    path("feed/", views.feed, name="feed"),
+    # path("learn/", views.learn, name="learn"),
+    # path("history/", views.history, name="history"),
+    # path("profile/", views.profile, name="profile"),
 ]
--- app/project/frontend/views.py
+++ app/project/frontend/views.py
@@ -8,5 +8,29 @@ def landing(request, *args, **kwargs):
     return render(request, "landing.html")
 
 
+def login(request, *args, **kwargs):
+    return render(request, "login.html")
+
+
+def signup(request, *args, **kwargs):
+    return render(request, "signup.html")
+
+
 def tennis(request, *args, **kwargs):
     return render(request, "tennis.html")
+
+
+def feed(request, *args, **kwargs):
+    return render(request, "feed.html")
+
+
+def learn(request, *args, **kwargs):
+    return render(request, "learn.html")
+
+
+def history(request, *args, **kwargs):
+    return render(request, "history.html")
+
+
+def profile(request, *args, **kwargs):
+    return render(request, "profile.html")
