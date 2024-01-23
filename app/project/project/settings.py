38811cdf59b0959a2854cdff8cecb402b131332c
Worthy <worthyintech@gmail.com>
2024-01-23 18:15:19 +0000

    Adding the urls.

--- app/project/project/settings.py
+++ app/project/project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'backend.apps.BackendConfig'
+    'backend.apps.BackendConfig',
+    'frontend.apps.FrontendConfig',
 ]
 
 MIDDLEWARE = [
