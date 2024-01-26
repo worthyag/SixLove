922bbaec6cfce842392f5c1af83b738742fbbffa
Worthy <worthyintech@gmail.com>
2024-01-26 12:07:18 +0000

    Updating progress.

--- algorithms/Plan.py
+++ algorithms/Plan.py
@@ -4,6 +4,10 @@ class Plan:
 
     Methods
     -------
+    rapid_fire()
+    steady_waters()
+    deep_dive()
+    plan_error()
     """
 
     def __init__(self, duration: str, is_daily: bool,
@@ -17,30 +21,40 @@ class Plan:
 
         match self.duration:
             case "6W":
-                self.rapid_fire
+                self.rapid_fire()
             case "3M":
-                self.steady_waters
+                self.steady_waters()
             case "6M":
-                self.deep_dive
+                self.deep_dive()
             case _:
-                self.plan_error
+                self.plan_error()
 
     def rapid_fire(self):
         """
         6 Weeks
         """
+        if self.train_days != None:
+            pass
+        else:
+            print("Hi I am RapidFire")
 
     def steady_waters(self):
         """
         3 Months
         """
-        pass
+        if self.train_days != None:
+            pass
+        else:
+            print("Hi I am SteadyWaters")
 
     def deep_dive(self):
         """
         6 Months
         """
-        pass
+        if self.train_days != None:
+            pass
+        else:
+            print("Hi I am DeepDive")
 
     def plan_error(self):
         """
@@ -51,7 +65,8 @@ class Plan:
 
 p = Plan("6W", True)
 # p = Plan("6W", False, "Mon,Wed,Fri")
-# p.rapid_fire()
+
+# p = Plan("3M", True)
 
 print(p.duration)
 print(p.is_daily)
