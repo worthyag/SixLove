57809dfdd010a9f64a9e76fd7848cc400e9a9d75
Worthy <worthyintech@gmail.com>
2024-01-26 11:50:59 +0000

    Updating progress.

--- algorithms/Plan.py
+++ algorithms/Plan.py
@@ -4,24 +4,26 @@ class Plan:
 
     Methods
     -------
-    1
-    2
-    3
-    4
-    5
-    6
-    7
-    8
-    9
     """
 
     def __init__(self, duration: str, is_daily: bool,
                  train_days: str = None):
+        """"""
         self.duration = duration
         self.is_daily = is_daily
+        # If the sessions are daily, then train_days doesn't need to be set.
+        self.train_days = train_days.split(
+            ",") if not self.is_daily else train_days
 
-        if not self.is_daily:
-            self.train_days = train_days.split()
+        match self.duration:
+            case "6W":
+                self.rapid_fire
+            case "3M":
+                self.steady_waters
+            case "6M":
+                self.deep_dive
+            case _:
+                self.plan_error
 
     def rapid_fire(self):
         """
@@ -40,8 +42,15 @@ class Plan:
         """
         pass
 
+    def plan_error(self):
+        """
+        Error handling method
+        """
+        print("Something went wrong.")
+
 
 p = Plan("6W", True)
+# p = Plan("6W", False, "Mon,Wed,Fri")
 # p.rapid_fire()
 
 print(p.duration)
