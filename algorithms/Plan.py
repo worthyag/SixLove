07ae9841ea4d0f4a80209f90f1e27db32145ab0b
Worthy <worthyintech@gmail.com>
2024-01-26 11:30:29 +0000

    Adding the files.

---
+++ algorithms/Plan.py
@@ -0,0 +1,37 @@
+class Plan:
+    """
+    Creates a different plan based on the given parameters.
+
+    Methods
+    -------
+    1
+    2
+    3
+    4
+    5
+    6
+    7
+    8
+    9
+    """
+
+    def __init__(self):
+        pass
+
+    def rapid_fire(self):
+        """
+        6 Weeks
+        """
+        pass
+
+    def steady_waters(self):
+        """
+        3 Months
+        """
+        pass
+
+    def deep_dive(self):
+        """
+        6 Months
+        """
+        pass
--- algorithms/buildPlan.py
+++ algorithms/buildPlan.py
@@ -1,12 +1,31 @@
 class BuildPlan:
-    """
-    x
+    """"""
 
-    Methods
-    -------
-    x
-    """
-
-    def __init__(self, df):
+    def __init__(self):
         """"""
-        self.df = df
+        pass
+
+    def test(self):
+        """
+        Tests that my class is working.
+        """
+        print("This is working.")
+
+    def name(self):
+        """
+        1. Receives the data.
+            - plan_duration
+            - training_frequency
+            - training_days
+        2. Functions that create the different plans.
+        These functions will handle, what date the 
+        plans start on, and what exercises are given.
+            - There are 9 different schedules.
+            - Based on the number of schedules, I will
+            most likely create a plan class, and each 
+            method of it builds a different schedule.
+        """
+
+
+bp = BuildPlan()
+bp.test()
