6e04b08308607b45d0bf7264c6c21c19ab113443
Worthy <worthyintech@gmail.com>
2024-01-31 12:01:25 +0000

    Updating progress.

--- tennis_session_crud_app/project/tennis_session/views.py
+++ tennis_session_crud_app/project/tennis_session/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpRequest
+from django.http import HttpResponse
 
 # Create your views here.
 
 
 def index(request):
-    return HttpRequest("This is the index page of the tennis_session app.")
+    return HttpResponse("This is the index page of the tennis_session app.")
