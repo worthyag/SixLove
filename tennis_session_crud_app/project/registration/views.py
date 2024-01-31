b3b33d9517eb87d4f019d1d7fe49dd1b8474d114
Worthy <worthyintech@gmail.com>
2024-01-31 20:10:53 +0000

    Working on user authentication.

--- tennis_session_crud_app/project/registration/views.py
+++ tennis_session_crud_app/project/registration/views.py
@@ -18,13 +18,29 @@ def signup(request):
 
     return render(request,
                   "./registration/signup.html",
-                  {"title": "Sign Up"}
+                  {
+                      "title": "Sign Up",
+                      "form": form
+                  }
                   )
 
 
 def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("tennis-sessions")
+    else:
+        form = AuthenticationForm()
+
     return render(
         request,
         "./registration/login.html",
-        {"title": "Login"}
+        {
+            "title": "Login",
+            "form": form
+        }
     )
