0acb7565af3e14b0f53515cfa5a7a96a8c918e37
Worthy <worthyintech@gmail.com>
2024-01-31 17:03:06 +0000

    Testing my code.

--- tennis_session_crud_app/project/tennis_session/tests.py
+++ tennis_session_crud_app/project/tennis_session/tests.py
@@ -8,8 +8,17 @@ from .models import TennisSession
 # Create your tests here.
 
 
+def create_tennis_session(date):
+    """
+    Creates a tennis session to reduce code redundancy / repetition 
+    when running tests.
+    """
+    tennis_session = TennisSession(date=date)
+
+
 class TennisSessionModelTests(TestCase):
-    # Testing my is_tennis_session_scheduled_today() method.
+    """Testing my is_tennis_session_scheduled_today() method."""
+
     def test_tennis_session_scheduled_today(self):
         """
         is_tennis_session_scheduled_today() returns True if a tennis session
@@ -34,7 +43,7 @@ class TennisSessionModelTests(TestCase):
 
     def test_date_in_string_format_tennis_session_scheduled_today(self):
         """
-        Test whether is_tennis_session_scheduled_today() returns True if a tennis session
+        Tests whether is_tennis_session_scheduled_today() returns True if a tennis session
         is scheduled today and the date is given in a string format.
         """
         date = timezone.now()
@@ -53,7 +62,7 @@ class TennisSessionModelTests(TestCase):
 
     def test_date_in_string_format_no_tennis_session_scheduled_today(self):
         """
-        Test whether is_tennis_session_scheduled_today() returns False if when no tennis session
+        Tests whether is_tennis_session_scheduled_today() returns False if when no tennis session
         is scheduled today and the date is given in a string format.
         """
         date = timezone.now() + datetime.timedelta(days=3)
@@ -69,3 +78,13 @@ class TennisSessionModelTests(TestCase):
         self.assertIs(
             future_tennis_session.is_tennis_session_scheduled_today(), False
         )
+
+
+class TennisSessionDeleteViewTests(TestCase):
+    """Testing the delete view."""
+
+    def test_tennis_sessions_removed_from_db_when_deleted(self):
+        """
+        Tests whether tennis sessions are removed from the database when
+        deleted.
+        """
