03d39393c3738a29bf6c0a08b469df5395d5d57f
Worthy <worthyintech@gmail.com>
2024-01-31 14:05:22 +0000

    Added a success page.

--- tennis_session_crud_app/project/tennis_session/forms.py
+++ tennis_session_crud_app/project/tennis_session/forms.py
@@ -1,1 +1,16 @@
 from django import forms
+from .models import TennisSession
+
+
+class AddTennisSessionForm(forms.ModelForm):
+    """
+    Creates a tennis session object / add a tennis session to the database.
+    """
+    # title = forms.CharField(max_length=150)
+    # notes = forms.Textarea()
+    # date = forms.DateTimeField()
+    # is_completed = forms.BooleanField()
+
+    class Meta:
+        model = TennisSession
+        fields = ['title', 'notes', 'date', 'is_completed']
--- tennis_session_crud_app/project/tennis_session/views.py
+++ tennis_session_crud_app/project/tennis_session/views.py
@@ -1,14 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # from django.http import HttpResponse
+
+from . import forms
 
 # Create your views here.
 
 
 def index(request):
+    if request.method == 'POST':
+        form = forms.AddTennisSessionForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("success")
+
+    # Initialising a new form.
+    form = forms.AddTennisSessionForm()
+
     return render(
         request,
         "./tennis_session/index.html",
         {
             "title": "Tennis Session",
+            "form": form,
         }
     )
+
+
+def success(request):
+    return render(request, "./tennis_session/success.html")
