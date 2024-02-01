399c0675965ff3668ca6a382e25e1bf44180f475
Worthy <worthyintech@gmail.com>
2024-02-01 13:31:46 +0000

    Updating the user creation form.

--- app/project/registration/models.py
+++ app/project/registration/models.py
@@ -7,5 +7,5 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     date_of_birth = models.DateField()
