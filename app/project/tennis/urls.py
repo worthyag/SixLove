dc930c67363b4b3ab15fdcfb8c22725dfbf7f2ac
Worthy <worthyintech@gmail.com>
2024-02-01 20:54:40 +0000

    Working on the functionality that only allows users to view their own sessions.

--- app/project/tennis/urls.py
+++ app/project/tennis/urls.py
@@ -7,4 +7,7 @@ app_name = "tennis"
 urlpatterns = [
     path("", views.tennis, name="tennis"),
     path("add/", views.add, name="add"),
+    path("<int:tennis_session_id>/edit/", views.add, name="edit"),
+    path("<int:tennis_session_id>/delete/", views.add, name="delete"),
+    path("success/", views.add, name="success"),
 ]
