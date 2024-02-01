dc1238d29b43f16da5f04770b38a861a30268585
Worthy <worthyintech@gmail.com>
2024-02-01 20:12:19 +0000

    Adding the form.

---
+++ app/project/tennis/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import TennisSession
+
+
+class TennisSessionForm(forms.ModelForm):
+    """
+    Creates (adds) / updates a tennis session object / tennis session to the database.
+    """
+    class Meta:
+        model = TennisSession
+        fields = ['title', 'notes', 'date', 'is_completed']
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'date'})
+        }
