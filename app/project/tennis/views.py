ce86e1c1ac3113d5ddedd38376f45539400f4f34
Worthy <worthyintech@gmail.com>
2024-02-01 20:39:32 +0000

    Making it so you have to be logged in to add sessions and view them.

--- app/project/tennis/views.py
+++ app/project/tennis/views.py
@@ -9,48 +9,38 @@ from . import models
 
 
 # Create your views here.
-
+@login_required
 def tennis(request):
     """"""
-    # tennis_sessions = models.TennisSession.objects.filter(
-    #     user=request.user).order_by("date")
+    tennis_sessions = models.TennisSession.objects.filter(
+        user=request.user).order_by("date")
 
-    # today_sessions = [
-    #     session for session in tennis_sessions if session.is_tennis_session_scheduled_today()]
-    # is_today = "No sessions scheduled for today." if len(
-    #     today_sessions) == 0 else ""
+    today_sessions = [
+        session for session in tennis_sessions if session.is_tennis_session_scheduled_today()]
+    is_today = "No sessions scheduled for today." if len(
+        today_sessions) == 0 else ""
 
-    # upcoming_sessions = [session for session in tennis_sessions
-    #                      if not session.is_tennis_session_scheduled_today() and
-    #                      session.date > datetime.date.today()]
+    upcoming_sessions = [session for session in tennis_sessions
+                         if not session.is_tennis_session_scheduled_today() and
+                         session.date > datetime.date.today()]
 
-    # past_sessions = [session for session in tennis_sessions
-    #                  if not session.is_tennis_session_scheduled_today() and
-    #                  session.date < datetime.date.today()]
-
-    # return render(
-    #     request,
-    #     "./tennis/tennis.html",
-    #     {
-    #         "title": "Tennis",
-    #         "today_sessions": today_sessions,
-    #         "is_today": is_today,
-    #         "upcoming_sessions": upcoming_sessions,
-    #         "past_sessions": past_sessions,
-    #     })
+    past_sessions = [session for session in tennis_sessions
+                     if not session.is_tennis_session_scheduled_today() and
+                     session.date < datetime.date.today()]
 
     return render(
         request,
         "./tennis/tennis.html",
         {
             "title": "Tennis",
-            # "today_sessions": today_sessions,
-            # "is_today": is_today,
-            # "upcoming_sessions": upcoming_sessions,
-            # "past_sessions": past_sessions,
+            "today_sessions": today_sessions,
+            "is_today": is_today,
+            "upcoming_sessions": upcoming_sessions,
+            "past_sessions": past_sessions,
         })
 
 
+@login_required
 def add(request):
     """"""
     if request.method == 'POST':
