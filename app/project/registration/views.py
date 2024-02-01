20d999d9e56c2e0396bebb288e9a9e1521460b1f
Worthy <worthyintech@gmail.com>
2024-02-01 13:00:01 +0000

    Adding the necessary systems.

--- app/project/registration/views.py
+++ app/project/registration/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
 
 
 def home(request):
+    """"""
     return render(
         request,
         "./registration/index.html",
@@ -14,26 +17,51 @@ def home(request):
 
 
 def user_login(request):
+    """"""
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = AuthenticationForm()
+
     return render(
         request,
         "./registration/login.html",
         {
-            "title": "Login"
+            "title": "Login",
+            "form": form
         }
     )
 
 
 def signup(request):
+    """"""
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = UserCreationForm()
+
     return render(
         request,
         "./registration/signup.html",
         {
-            "title": "Sign Up"
+            "title": "Sign Up",
+            "form": form
         }
     )
 
 
 def onboarding(request):
+    """"""
     return render(
         request,
         "./registration/onboarding.html",
