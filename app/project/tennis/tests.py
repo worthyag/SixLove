5592c5a455adc271f1915d592f21225857f66b0e
Worthy <worthyintech@gmail.com>
2024-02-03 13:23:15 +0000

    Updating progress.

--- app/project/tennis/tests.py
+++ app/project/tennis/tests.py
@@ -23,8 +23,11 @@ class TennisViewsTest(TestCase):
             password="testpassword"
         )
 
-    def test_tennis_view_with_no_tennis_sessions(self):
-        """"""
+    def helper_get_response_tennis_view(self):
+        """
+        A helper function that produces a response for the tennis view.
+        (To reduce code repetition).
+        """
         # Creating an instance of a GET request.
         request = self.factory.get(reverse("tennis:tennis"))
 
@@ -33,6 +36,13 @@ class TennisViewsTest(TestCase):
 
         # Testing the view.
         response = views.tennis(request)
+
+        return response
+
+    def test_tennis_view_with_no_tennis_sessions(self):
+        """"""
+        # Generating the response.
+        response = self.helper_get_response_tennis_view()
 
         self.assertEqual(response.status_code, 200)
         self.assertContains(
@@ -53,14 +63,8 @@ class TennisViewsTest(TestCase):
             date=today,
         )
 
-        # Creating an instance of a GET request.
-        request = self.factory.get(reverse("tennis:tennis"))
-
-        # Simulating a logged-in user manually.
-        request.user = self.user
-
-        # Testing the view.
-        response = views.tennis(request)
+        # Generating the response.
+        response = self.helper_get_response_tennis_view()
 
         self.assertEqual(response.status_code, 200)
         self.assertNotContains(
@@ -68,15 +72,37 @@ class TennisViewsTest(TestCase):
             "No tennis sessions scheduled for today."
         )
 
-# class AddViewTest(TestCase):
-#     """Testing the add view work as expected."""
-#     def setUp(self) -> None:
-#         """"""
-#         # Every test needs access to the request factory.
-#         self.factory = RequestFactory()
-#         self.user = CustomUser.objects.create(
-#             username="testuser",
-#             password="testpassword"
-#         )
 
-#     # def
+class AddViewTest(TestCase):
+    """Testing the add view work as expected."""
+
+    def setUp(self) -> None:
+        """"""
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+        self.user = CustomUser.objects.create(
+            username="testuser",
+            password="testpassword"
+        )
+
+    def helper_get_response_add_view(self):
+        """
+        A helper function that produces a response for the add view.
+        """
+        # Creating an instance of a GET request.
+        request = self.factory.get(reverse("tennis:add"))
+
+        # Simulating a logged-in user manually.
+        request.user = self.user
+
+        # Testing the view.
+        response = views.add(request)
+
+        return response
+
+    def test_add_view_loads(self):
+        """"""
+        # Generating the response.
+        response = self.helper_get_response_add_view()
+
+        self.assertEqual(response.status_code, 200)
