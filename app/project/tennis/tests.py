2efd3fdea5ab18561f213dea6f6a9c11ec198edd
Worthy <worthyintech@gmail.com>
2024-02-03 12:55:59 +0000

    Testing the tennis app.

--- app/project/tennis/tests.py
+++ app/project/tennis/tests.py
@@ -1,9 +1,12 @@
 import datetime
 
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from django.urls import reverse
 # from django.contrib.auth.models import Users
+# from django.contrib.auth.views import login
+
 from registration.models import CustomUser
+from . import views
 
 # Create your tests here.
 
@@ -13,6 +16,8 @@ class TennisViewsTest(TestCase):
 
     def setUp(self) -> None:
         """"""
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
         self.user = CustomUser.objects.create(
             username="testuser",
             password="testpassword"
@@ -20,12 +25,17 @@ class TennisViewsTest(TestCase):
 
     def test_tennis_view_with_no_tennis_sessions(self):
         """"""
-        self.client.login(username=self.user.username,
-                          password=self.user.password)
+        # Creating an instance of a GET request.
+        request = self.factory.get(reverse("tennis:tennis"))
 
-        response = self.client.get(reverse("tennis:tennis"))
-        self.assertEqual(response.status_code, 302)
-        self.assertContains(response, "No tennis sessions scheduled for today.")
+        # Simulating a logged-in user manually.
+        request.user = self.user
 
+        # Testing the view.
+        response = views.tennis(request)
 
-
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(
+            response,
+            "No tennis sessions scheduled for today."
+        )
