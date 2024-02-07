36f759a8b318a8aba4a24ec88426a2ab046e4537
Worthy <worthyintech@gmail.com>
2024-02-07 16:01:50 +0000

    Updating progress.

--- app/project/planner/urls.py
+++ app/project/planner/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = "planner"
 
 urlpatterns = [
-    path("", views.calendar, name="calendar"),
-    path("<int:year>/<int:month>/", views.calendar, name="calendar"),
+    # path("", views.calendar, name="calendar"),
+    path("", views.CalendarView.as_view(), name="calendar"),
+    # path("<int:year>/<int:month>/", views.calendar, name="calendar"),
 ]
--- app/project/planner/views.py
+++ app/project/planner/views.py
@@ -8,6 +8,8 @@ from django.utils.safestring import mark_safe
 
 from .utils import TennisCalendar
 
+from tennis import models as TennisModels
+
 
 # Create your views here.
 
@@ -65,33 +67,68 @@ from .utils import TennisCalendar
 
 
 @login_required
-def calendar(request, year=None, month=None):
+class CalendarView(generic.ListView):
     """"""
-    tennis_sessions = TennisModels.TennisSession.objects.filter(
-        user=request.user
-    )
-    cal = TennisCalendar(tennis_sessions, user=request.user)
+    model = TennisModels.TennisSession
+    template_name = "./planner/calendar.html"
 
-    month, year = month if month else timezone.now().date(
-    ).month, year if year else timezone.now().date().year
+    def setup(self, request, *args, **kwargs):
+        super().setup(request, *args, **kwargs)
 
-    # Assuming you have a method to convert your TennisSession objects into a format suitable for the calendar
-    tennis_sessions = [
-        {
-            'title': tennis_session.title,
-            'day': tennis_session.date.day if type(tennis_session.date) != type("") else tennis_session.date[8:11]
-        } for tennis_session in tennis_sessions]
+        self.user = request.user
 
-    # html_cal = cal.formatmonth(int(year), int(month), withyear=True, tennis_sessions=tennis_sessions)
+    def get_context_date(self, **kwargs):
+        """"""
+        context = super().get_context_data(**kwargs)
 
-    return render(
-        request,
-        "./planner/calendar.html",
-        {
-            "title": "Calendar",
-            "year": year,
-            "month": month,
-            "calendar": cal,
-            'tennis_sessions': tennis_sessions
-        }
-    )
+        # Using today's date for the calendar.
+        today = get_date(self.request.GET.get('day', None))
+
+        # Instantiating the tennis calendar class with today's year and date.
+        cal = TennisCalendar(today.year, today.month, self.user)
+
+        # Calling the formatmonth method, which returns our calendar as a table.
+        html_cal = cal.formatmonth(withyear=True)
+        context['calendar'] = mark_safe(html_cal)
+        return context
+
+
+def get_date(req_day):
+    """"""
+    if req_day:
+        year, month = (int(x) for x in req_day.split('-'))
+        return datetime.date(year, month, day=1)
+    return timezone.now().date()
+
+
+# @login_required
+# def calendar(request, year=None, month=None):
+#     """"""
+#     tennis_sessions = TennisModels.TennisSession.objects.filter(
+#         user=request.user
+#     )
+#     cal = TennisCalendar(tennis_sessions, user=request.user)
+
+#     month, year = month if month else timezone.now().date(
+#     ).month, year if year else timezone.now().date().year
+
+#     # Assuming you have a method to convert your TennisSession objects into a format suitable for the calendar
+#     tennis_sessions = [
+#         {
+#             'title': tennis_session.title,
+#             'day': tennis_session.date.day if type(tennis_session.date) != type("") else tennis_session.date[8:11]
+#         } for tennis_session in tennis_sessions]
+
+#     # html_cal = cal.formatmonth(int(year), int(month), withyear=True, tennis_sessions=tennis_sessions)
+
+#     return render(
+#         request,
+#         "./planner/calendar.html",
+#         {
+#             "title": "Calendar",
+#             "year": year,
+#             "month": month,
+#             "calendar": cal,
+#             'tennis_sessions': tennis_sessions
+#         }
+#     )
