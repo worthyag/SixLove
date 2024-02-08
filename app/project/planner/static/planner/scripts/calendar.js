641e5b2c04da6b5eeb5be1b9ebb5361c7233e449
Worthy <worthyintech@gmail.com>
2024-02-08 16:48:18 +0000

    Finished main calendar functionality.

--- app/project/planner/static/planner/scripts/calendar.js
+++ app/project/planner/static/planner/scripts/calendar.js
@@ -14,9 +14,6 @@ let activeDay;
 let year = today.getFullYear();
 let month = today.getMonth();
 
-print(year);
-print(month);
-
 function getMonth(month) {
   month = month + 1;
 
@@ -81,114 +78,38 @@ function buildCalendar() {
   daysDiv.innerHTML = days;
 }
 
+buildCalendar();
+
+function prevMonth() {
+  /** */
+  month--;
+
+  if (month < 0) {
+    month = 11;
+    year--;
+  }
+
+  buildCalendar();
+}
+
+function nextMonth() {
+  /** */
+  month++;
+
+  if (month > 11) {
+    month = 0;
+    year++;
+  }
+
+  buildCalendar();
+}
+
+// Adding event listeners to the prevBtn and nextBtn.
+prevBtn.addEventListener('click', prevMonth);
+nextBtn.addEventListener('click', nextMonth);
 
 
 // TODO: Turn these functions into a class, with the calendar variable as an attribute.
 
 
-function getDayOfWeek(day) {
-  const daysOfWeek = {
-    1: "Monday",
-    2: "Tuesday",
-    3: "Wednesday",
-    4: "Thursday",
-    5: "Friday",
-    6: "Saturday",
-    7: "Sunday"
-  };
 
-  return [day, daysOfWeek[day]];
-}
-
-function getDateInfo(date) {
-  const date_info = {
-    "date": date,
-    "year": date.getFullYear(),
-    "month": getMonth(date.getMonth()),
-    "dayOfWeek": getDayOfWeek(date.getDay()),
-    "day": date.getDate()
-
-  };
-
-  return date_info;
-}
-
-function checkLeapYear(year) {
-  // Three conditions to find out the leap year.
-  if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
-      return true;
-  } else {
-      return false;
-  }
-}
-
-function getNumberOfDays(month, year) {
-  const months = {
-    1: 31,
-    2: [28, 29],
-    3: 31,
-    4: 30,
-    5: 31,
-    6: 30,
-    7: 31,
-    8: 31,
-    9: 30,
-    10: 31,
-    11: 30,
-    12: 31,
-  };
-
-  if (month !== 2) 
-    return months[month];
-  else if (checkLeapYear(year))
-    return months[month][1]
-  else
-    return months[month][0]
-}
-
-function buildCalendar2(date = "today") {
-  const today = new Date();
-  let date_info = "";
-
-  if (date !== "today") {
-    date_info = getDateInfo(date);
-  }
-  else {
-    date_info = getDateInfo(today);
-    print(date_info);
-  }
-
-  const numberOfDays = getNumberOfDays(date_info["month"][0], date_info["year"]);
-  print(numberOfDays);
-
-  const calendarDiv = document.createElement("div");
-  calendarDiv.classList.add("calendarDiv");
-
-  for (let i = 0; i < numberOfDays; i++) {
-    const div = document.createElement("div");
-    div.classList.add("dayDiv");
-
-    const day = document.createElement("p");
-    day.textContent = i + 1;
-    day.classList.add("day");
-
-    // Fix this.
-    if (((i + 1) === date_info["day"]) && 
-    (getDateInfo(today)["month"] == date_info["month"])) {
-      day.classList.add("currentDay");
-    }
-
-    div.appendChild(day);
-
-    // For testing
-    const task1 = document.createElement("div");
-    task1.classList.add("task");
-    div.appendChild(task1);
-
-    calendarDiv.appendChild(div);
-  }
-
-  calendar.appendChild(calendarDiv);
-}
-
-buildCalendar();
