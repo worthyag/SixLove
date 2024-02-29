551932531574375f7ff5b403849f814619525c36
Worthy <worthyintech@gmail.com>
2024-02-29 16:42:14 +0000

    Working on the feed.

--- app/project/community/static/community/scripts/create-post.js
+++ app/project/community/static/community/scripts/create-post.js
@@ -1,17 +1,12 @@
-console.log("Add posts!");
+const createPostBtn = document.querySelector(".create-post-btn");
 
-// const feed_posts = document.querySelectorAll(".feed-post");
-
-// for (const feed_post of feed_posts) {
-//   feed_post.addEventListener("click", (post) => {
-//     console.log("You clicked me!");
-
-//     viewPost(post);
-//   });
-// }
+createPostBtn.addEventListener("click", () => {
+  console.log("You clicked me!");
+  createPost();
+});
 
 
-// function viewPost(post) {
-//   const modal = document.querySelector("#viewPostModal");
-//   modal.style.display = "block";
-// }
+function createPost() {
+  const modal = document.querySelector("#createPostModal");
+  modal.style.display = "block";
+}
