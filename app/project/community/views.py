93ac405c5ec3031c8c0eac09ea20449cd31f6534
Worthy <worthyintech@gmail.com>
2024-02-29 15:12:38 +0000

    Updating progress.

--- app/project/community/views.py
+++ app/project/community/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+from . import models
 
 # Create your views here.
 
 
+@login_required
 def connect(request):
     """"""
     return render(
@@ -14,6 +18,7 @@ def connect(request):
     )
 
 
+@login_required
 def feed(request):
     """"""
     return render(
@@ -25,12 +30,23 @@ def feed(request):
     )
 
 
+@login_required
 def profile(request):
     """"""
+    try:
+        user_profile = models.UserProfile.objects.get(user=request.user)
+        user_posts = models.UserPosts.objects.filter(
+            user=request.user).order_by('-created_at')
+    except:
+        user_profile = None
+        user_posts = None
+
     return render(
         request,
         "./community/profile.html",
         {
             "title": "Profile",
+            "user_profile": user_profile,
+            "user_posts": user_posts
         }
     )
