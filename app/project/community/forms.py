f63f0b8016948974e9388224ae68701bb787fefc
Worthy <worthyintech@gmail.com>
2024-02-29 19:22:00 +0000

    Updating progress.

--- app/project/community/forms.py
+++ app/project/community/forms.py
@@ -7,19 +7,20 @@ class UserPostsForm(forms.ModelForm):
     Creates (adds) / updates a user post to the database.
     """
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['user_profile'].required = False
+
     class Meta:
         model = UserPosts
         fields = ['user_profile', 'post_picture', 'post_caption', 'created_at']
+
+        # self.fields['user_profile'].required = False
 
         widgets = {
             'user_profile': forms.HiddenInput(),
             'created_at': forms.HiddenInput(),
         }
-
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            # Set the field as not required
-            self.fields['user_profile'].required = False
 
 
 class UserProfileForm(forms.ModelForm):
