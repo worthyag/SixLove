7bd2486107c48268307be9de740aee917d714520
Worthy <worthyintech@gmail.com>
2024-02-29 09:02:14 +0000

    Updating progress.

--- app/project/community/static/community/scripts/toggle-posts.js
+++ app/project/community/static/community/scripts/toggle-posts.js
@@ -1,1 +1,20 @@
-console.log("Am I working!");
+const postsBtn = document.querySelector(".posts-btn");
+const awardsBtn = document.querySelector(".achievements-btn");
+
+const postsDiv = document.querySelector(".feed");
+const awardsDiv = document.querySelector(".achievements");
+
+
+postsBtn.addEventListener("click", () => {
+  console.log("Posts button clicked!");
+
+  postsDiv.classList.remove("toggle-posts");
+  awardsDiv.classList.add("toggle-posts");
+});
+
+awardsBtn.addEventListener("click", () => {
+  console.log("Awards button clicked!");
+
+  awardsDiv.classList.remove("toggle-posts");
+  postsDiv.classList.add("toggle-posts");
+});
