86d3176ad67cf17969eaf5a1b322cb602e624980
Worthy <worthyintech@gmail.com>
2024-02-29 13:02:49 +0000

    Updating progress.

--- app/project/community/static/community/scripts/toggle-like.js
+++ app/project/community/static/community/scripts/toggle-like.js
@@ -1,1 +1,15 @@
-console.log("Am I working (toggle like)?");
+const likeButtons = document.querySelectorAll(".post-like-btn");
+
+for (const likeButton of likeButtons) {
+  likeButton.addEventListener("click", () => {
+    const img = likeButton.children[0];
+    const url = img.getAttribute("src");
+
+    const liked = "/static/community/images/liked-icon.svg";
+    const like = "/static/community/images/like-icon.svg";
+
+    img.setAttribute("src", (url === like) ? liked : like);
+  
+  });
+}
+
