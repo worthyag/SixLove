1131f894f0c7d25372c220d6c51825427efb92e6
Worthy <worthyintech@gmail.com>
2024-02-29 13:26:20 +0000

    Updating progress.

--- app/project/community/models.py
+++ app/project/community/models.py
@@ -1,4 +1,32 @@
 from django.db import models
-from registration.models import CustomUser
+from django.contrib.auth import get_user_model
+from django.utils import timezone
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    """"""
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    profile_picture = models.ImageField(
+        upload_to='profile_pics/',
+        default='./community/images/profile-pic-temp.png'
+    )
+    bio = models.TextField(blank=True)
+    username = models.CharField(max_length=50)
+    profile_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.username
+
+
+class UserPosts(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    post_picture = models.ImageField(
+        upload_to='post_pics/'
+    )
+    post_caption = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now())
+
+    def __str__(self):
+        return f"{self.user.username}'s post - {self.created_at}"
