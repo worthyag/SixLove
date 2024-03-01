f7b23bd38f81c5435aa8fb1418f76842344bed97
Worthy <worthyintech@gmail.com>
2024-03-01 10:19:33 +0000

    Updating progress.

--- app/project/community/static/community/scripts/edit-delete-post.js
+++ app/project/community/static/community/scripts/edit-delete-post.js
@@ -3,26 +3,38 @@ const deletePostButtons = document.querySelectorAll(".delete-post-btn");
 
 
 for (const editPostBtn of editPostButtons) {
-  editPostBtn.addEventListener("click", () => {
+  editPostBtn.addEventListener("click", (e) => {
     console.log("You clicked edit!");
-    editPost();
+    editPost(e.target);
   });
 }
 
 for (const deletePostBtn of deletePostButtons) {
-  deletePostBtn.addEventListener("click", () => {
+  deletePostBtn.addEventListener("click", (e) => {
     console.log("You clicked delete!");
-    deletePost();
+    deletePost(e.target);
   });
 }
 
 
-function editPost() {
+function editPost(btn) {
   const modal = document.querySelector("#editPostModal");
+  const postToEdit = document.querySelector("#post-id-to-edit");
+  const post_id = btn.parentElement.parentElement
+                  .parentElement.nextElementSibling.children[0].textContent;
+
+  postToEdit.value = post_id;
+
   modal.style.display = "block";
 }
 
-function deletePost() {
+function deletePost(btn) {
   const modal = document.querySelector("#deletePostModal");
+  const postToDelete = document.querySelector("#post-id-to-delete");
+  const post_id = btn.parentElement.parentElement
+                  .parentElement.nextElementSibling.children[0].textContent;
+
+  postToDelete.value = post_id;
+
   modal.style.display = "block";
 }
