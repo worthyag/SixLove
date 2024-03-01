a504d5df3fd32527e935443b2f4dd998fbbf1140
Worthy <worthyintech@gmail.com>
2024-03-01 08:41:45 +0000

    Updating progress.

--- app/project/community/static/community/scripts/post-menu.js
+++ app/project/community/static/community/scripts/post-menu.js
@@ -1,1 +1,21 @@
-console.log("Post menu...");
+const postMoreBtn =  document.querySelectorAll(".post-more-btn");
+
+for (const btn of postMoreBtn) {
+  btn.addEventListener("click", (e) => {
+    displayActionMenu(e.target, btn.nextElementSibling);
+  });
+}
+
+function displayActionMenu(btn, div) {
+  const moreURL = "/static/community/images/more-icon.svg";
+  const closeURL = "/static/community/images/close-icon.svg";
+
+  if (btn.getAttribute("src") === moreURL) {
+    btn.setAttribute("src", closeURL);
+    div.classList.remove("hide-action-btns");
+  } 
+  else {
+    btn.setAttribute("src", moreURL);
+    div.classList.add("hide-action-btns");
+  }
+}
