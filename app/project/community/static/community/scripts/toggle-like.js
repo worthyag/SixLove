609cc2b9246bfa91e17f62aaced075e1bed3cc0e
Worthy <worthyintech@gmail.com>
2024-03-02 10:56:46 +0000

    Updating progress.

--- app/project/community/static/community/scripts/toggle-like.js
+++ app/project/community/static/community/scripts/toggle-like.js
@@ -1,15 +1,47 @@
 const likeButtons = document.querySelectorAll(".post-like-btn");
 
 for (const likeButton of likeButtons) {
-  likeButton.addEventListener("click", () => {
-    const img = likeButton.children[0];
-    const url = img.getAttribute("src");
+  likeButton.addEventListener("click", async () => {
+    const postId = likeButton.closest(".feed-post")
+                              .querySelector(".post-id-data")
+                              .innerText;
 
-    const liked = "/static/community/images/liked-icon.svg";
-    const like = "/static/community/images/like-icon.svg";
+    try {
+      const response = await fetch(`/toggle-like/${postId}/`, {method: "POST" });
+      const data = await response.json();
 
-    img.setAttribute("src", (url === like) ? liked : like);
-  
+      // Updating the like count.
+      const likeCountElement = likeButton.closest(".feed-post")
+                                          .querySelector(".like-count");
+      likeCountElement.innerText = data.like_count;
+
+      // Updating the like button image.
+      const img = likeButton.children[0];
+      const url = img.getAttribute("src");
+
+      const liked = "/static/community/images/liked-icon.svg";
+      const like = "/static/community/images/like-icon.svg";
+
+      img.setAttribute("src", (url === like) ? liked : like);
+
+    } catch (error) {
+        console.error("Error toggling the like: ", error);
+    } 
   });
 }
+
+
+
+// for (const likeButton of likeButtons) {
+//   likeButton.addEventListener("click", () => {
+//     const img = likeButton.children[0];
+//     const url = img.getAttribute("src");
+
+//     const liked = "/static/community/images/liked-icon.svg";
+//     const like = "/static/community/images/like-icon.svg";
+
+//     img.setAttribute("src", (url === like) ? liked : like);
+  
+//   });
+// }
 
