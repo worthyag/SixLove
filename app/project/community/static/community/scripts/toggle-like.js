ea95102594af2f031667c9076a1f653df17dcd14
Worthy <worthyintech@gmail.com>
2024-03-02 11:35:39 +0000

    Updating progress.

--- app/project/community/static/community/scripts/toggle-like.js
+++ app/project/community/static/community/scripts/toggle-like.js
@@ -43,18 +43,3 @@ for (const likeButton of likeButtons) {
   });
 }
 
-
-
-// for (const likeButton of likeButtons) {
-//   likeButton.addEventListener("click", () => {
-//     const img = likeButton.children[0];
-//     const url = img.getAttribute("src");
-
-//     const liked = "/static/community/images/liked-icon.svg";
-//     const like = "/static/community/images/like-icon.svg";
-
-//     img.setAttribute("src", (url === like) ? liked : like);
-  
-//   });
-// }
-
