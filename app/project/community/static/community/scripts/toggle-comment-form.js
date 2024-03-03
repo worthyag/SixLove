ad8f03d2d9a5a15e9c526a525c6924ed4b71eac0
Worthy <worthyintech@gmail.com>
2024-03-03 14:56:54 +0000

    Updating progress.

--- app/project/community/static/community/scripts/toggle-comment-form.js
+++ app/project/community/static/community/scripts/toggle-comment-form.js
@@ -1,1 +1,14 @@
-console.log("Toggle comment form!");
+const commentButtons = document.querySelectorAll(".post-comment-btn");
+
+for (const commentButton of commentButtons) {
+  console.log(commentButton);
+
+  commentButton.addEventListener("click", () => {
+    const postId = commentButton.closest(".post")
+                              .querySelector(".post-id-details")
+                              .children[0]
+                              .innerText;
+
+    console.log(`You clicked me! ${postId}`);                       
+  })
+}
