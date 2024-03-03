ff3844526b68df2eff109e649f84cd0310a2fad4
Worthy <worthyintech@gmail.com>
2024-03-03 10:28:01 +0000

    Updating progress.

--- app/project/community/urls.py
+++ app/project/community/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("feed/", views.feed, name="feed"),
     path("profile/", views.profile, name="profile"),
     path("user/<int:user_profile_id>", views.user, name="user"),
+    path("user/<int:user_profile_id>/", views.user, name="user"),
     path("toggle-like/<int:post_id>", views.toggle_like, name="toggle-like"),
     path("get-like-info/<int:post_id>",
          views.get_like_info, name="get-like-info"),
