893ffa2e0b3f6276e0408fde0f8682e33f5ed074
Worthy <worthyintech@gmail.com>
2024-03-08 17:19:52 +0000

    Updating progress.

--- app/project/tennis/views.py
+++ app/project/tennis/views.py
@@ -3,7 +3,7 @@ import datetime
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
-from django.db.models import Exists, OuterRef, Q
+from django.db.models import Exists, OuterRef, Q, Max, Count
 
 from . import forms
 from . import models
@@ -143,14 +143,52 @@ def success(request):
 def learn(request):
     # resources = models.Resource.objects.all().order_by("title")
 
-    # To allow the user to search through the list of users.
+    # To allow the user to search through the list of resources.
     # Getting the search query from the request.
     query = request.GET.get("resource-search", "")
+
+    # To allow the user to filter the resources.
+    filter_option = request.GET.get("filter", "")
 
     # Querying the data.
     resources = models.Resource.objects.filter(
         Q(title__icontains=query) | Q(tags__name__icontains=query)
     )
+
+    # Applying additional filters based on the filter_option.
+    if filter_option == "fundamentals":
+        resources = resources.filter(
+            tags__name__in={"forehand", "backhand", "serve"}
+        ).order_by("title")
+
+    elif filter_option == "fitness":
+        resources = resources.filter(
+            tags__name__in=["agility", "stretching"]
+        ).order_by("title")
+
+    elif filter_option == "stretch":
+        resources = resources.filter(
+            tags__name="stretching"
+        ).order_by("title")
+
+    elif filter_option == "forehand":
+        resources = resources.filter(
+            tags__name="forehand"
+        ).order_by("title")
+
+    elif filter_option == "backhand":
+        resources = resources.filter(
+            tags__name="backhand"
+        ).order_by("title")
+
+    elif filter_option == "volley":
+        resources = resources.filter(
+            tags__name="volley"
+        ).order_by("title")
+    elif filter_option == "slice":
+        resources = resources.filter(
+            tags__name="slice"
+        ).order_by("title")
 
     return render(
         request,
@@ -159,6 +197,7 @@ def learn(request):
             "title": "Learn",
             "resources": resources,
             "search_query": query,
+            "filter_option": filter_option,
         }
     )
 
