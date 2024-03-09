e2507c2c21af99891d32daa973bffbd90d1064f0
Worthy <worthyintech@gmail.com>
2024-03-09 07:36:59 +0000

    Updating progress.

--- app/project/community/context_processors.py
+++ app/project/community/context_processors.py
@@ -3,4 +3,4 @@ from .models import UserProfile
 
 def user_profile(request):
     # Return a dictionary with the user_profile
-    return {'user_profile': request.user.userprofile if request.user.is_authenticated else None}
+    return {'logged_profile': request.user.userprofile if request.user.is_authenticated else None}
