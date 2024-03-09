a35cbd41c82a7bab5b2e15e64fa2f50457b3d09a
Worthy <worthyintech@gmail.com>
2024-03-09 18:23:04 +0000

    Updating progress.

--- app/project/community/admin.py
+++ app/project/community/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import UserProfile, UserPosts
+from .models import UserProfile, UserPosts, Achievement, AchievementCategory
 
 # Register your models here.
 admin.site.register(UserProfile)
 admin.site.register(UserPosts)
+admin.site.register(AchievementCategory)
+admin.site.register(Achievement)
