d6a9d9425f6135f19381f5c8dd6fbe9aca789c19
Worthy <worthyintech@gmail.com>
2024-03-09 20:47:52 +0000

    Updating progress.

--- app/project/community/signals.py
+++ app/project/community/signals.py
@@ -11,8 +11,8 @@ def check_tennis_session_achievements(sender, instance, **kwargs):
     user_profile = instance.user
     # Check number of completed tennis sessions achievements
     check_achievement(user_profile,
-                      "award_category_name",
-                      "Number of completed Tennis Sessions",
+                      "Tennis Sessions",
+                      "Tennis Sessions",
                       [1, 5, 25, 50, 100])
 
     # Check category-specific achievements
@@ -34,10 +34,10 @@ def check_achievement(user_profile, award_category_name, name, levels):
     Check and award achievements for a specific task.
     """
     current_count = get_current_count(
-        user_profile, name)  # Implement this function
+        user_profile, award_category_name)  # Implement this function
 
     award_category = models.AchievementCategory.objects.get(
-        name=award_category_name,
+        name=award_category_name
     )
 
     for level, threshold in enumerate(levels, start=1):
@@ -45,12 +45,27 @@ def check_achievement(user_profile, award_category_name, name, levels):
             award_achievement(user_profile, award_category, name, level)
 
 
-def get_current_count(user_profile, name):
+def get_current_count(user_profile, award_category):
     """
     Get the current count for a specific task.
     """
     # Implement logic to get the current count based on the task (e.g., completed sessions, posts, likes, comments, follows, followers)
     # ...
+
+    if award_category == "Tennis Sessions":
+        pass
+    elif award_category == "Created Posts":
+        pass
+    elif award_category == "Liked Posts":
+        pass
+    elif award_category == "Comment Count":
+        pass
+    elif award_category == "Followed Count":
+        pass
+    elif award_category == "Followers Count":
+        pass
+    elif award_category == "Resources Read":
+        pass
 
 
 def award_achievement(user_profile, award_category, name, level):
@@ -60,7 +75,7 @@ def award_achievement(user_profile, award_category, name, level):
     achievement = models.Achievement.objects.create(
         user_profile=user_profile,
         category=award_category,
-        name=name,
+        name=f"{name} Lvl {level}",
         description=f"Achieved Level {level} for {name}.",
         level=level
     )
