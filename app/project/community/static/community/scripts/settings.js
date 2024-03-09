97cb861f010183bc00be0724972bbba50b58f32b
Worthy <worthyintech@gmail.com>
2024-03-09 12:27:51 +0000

    Updating progress.

--- app/project/community/static/community/scripts/settings.js
+++ app/project/community/static/community/scripts/settings.js
@@ -1,1 +1,5 @@
-console.log("Sister sister");
+const profileSettingsDiv = document.querySelector(".profile-settings-block");
+
+profileSettingsDiv.addEventListener("click", () => {
+  console.log("How do you feel?");
+})
