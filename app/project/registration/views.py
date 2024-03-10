865408721f075935a0c749ab852414e9496067bb
Worthy <worthyintech@gmail.com>
2024-03-10 02:23:46 +0000

    Updating progress.

--- app/project/registration/views.py
+++ app/project/registration/views.py
@@ -28,12 +28,14 @@ def home(request):
 
     elif filter_option == "not_completed":
         tennis_sessions = tennis_sessions.filter(is_completed=False)
-        
+
     elif filter_option == "upcoming":
-        tennis_sessions = tennis_sessions.filter(date__gt=timezone.now().date())
-        
+        tennis_sessions = tennis_sessions.filter(
+            date__gt=timezone.now().date())
+
     elif filter_option == "past":
-        tennis_sessions = tennis_sessions.filter(date__lt=timezone.now().date())
+        tennis_sessions = tennis_sessions.filter(
+            date__lt=timezone.now().date())
 
     forehand_sessions = tennis_sessions.filter(category="forehand")
     backhand_sessions = tennis_sessions.filter(category="backhand")
@@ -45,6 +47,23 @@ def home(request):
     agility_sessions = tennis_sessions.filter(category="agility")
     stamina_sessions = tennis_sessions.filter(category="stamina")
     other_sessions = tennis_sessions.filter(category="other")
+
+    tennis_sessions_per_month = TennisModels.TennisSession.objects.filter(
+        user=request.user
+    )
+
+    jan_sessions = tennis_sessions_per_month.filter(date__month=1)
+    feb_sessions = tennis_sessions_per_month.filter(date__month=2)
+    mar_sessions = tennis_sessions_per_month.filter(date__month=3)
+    apr_sessions = tennis_sessions_per_month.filter(date__month=4)
+    may_sessions = tennis_sessions_per_month.filter(date__month=5)
+    jun_sessions = tennis_sessions_per_month.filter(date__month=6)
+    jul_sessions = tennis_sessions_per_month.filter(date__month=7)
+    aug_sessions = tennis_sessions_per_month.filter(date__month=8)
+    sep_sessions = tennis_sessions_per_month.filter(date__month=9)
+    oct_sessions = tennis_sessions_per_month.filter(date__month=10)
+    nov_sessions = tennis_sessions_per_month.filter(date__month=11)
+    dec_sessions = tennis_sessions_per_month.filter(date__month=12)
 
     return render(
         request,
@@ -63,6 +82,18 @@ def home(request):
             "agility_sessions": agility_sessions,
             "stamina_sessions": stamina_sessions,
             "other_sessions": other_sessions,
+            "jan_sessions": jan_sessions,
+            "feb_sessions": feb_sessions,
+            "mar_sessions": mar_sessions,
+            "apr_sessions": apr_sessions,
+            "may_sessions": may_sessions,
+            "jun_sessions": jun_sessions,
+            "jul_sessions": jul_sessions,
+            "aug_sessions": aug_sessions,
+            "sep_sessions": sep_sessions,
+            "oct_sessions": oct_sessions,
+            "nov_sessions": nov_sessions,
+            "dec_sessions": dec_sessions,
         }
     )
 
